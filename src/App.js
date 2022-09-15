2007844d35540f2d4b0a0fdc52030c31b0736b72
Jos Codes <joselineruiz917@gmail.com>
2022-09-15 14:40:01 -0500

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import React,{useEffect, useState} from "react";
 import './App.css';
 
 const App = () => {
-  const APP_ID = 'b7d613fc';
-  const APP_KEY = 'ed856da4e53489cbcaab4eacf4aaf963';
+  const APP_ID = "b7d613fc";
+  const APP_KEY = "ed856da4e53489cbcaab4eacf4aaf963";
   
-  const exampleReq = "https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=b7d613fc&app_key=ed856da4e53489cbcaab4eacf4aaf963"
-  return(
+  const exampleReq = `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  
+  const [counter, setCounter] = useState(0);
+
+
+  useEffect(() => {
+    console.log("Effect has been run");
+  }, []);
+  
+  return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
   );
 };
